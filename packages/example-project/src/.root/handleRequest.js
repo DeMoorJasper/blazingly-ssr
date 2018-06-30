aa7156eb61d2a6a0d7ed8df8840fcc71e9b0b818
Jasper De Moor <jasperdemoor@gmail.com>
2018-06-30 09:19:40 +0200

    fix example

--- packages/example-project/src/.root/handleRequest.js
+++ packages/example-project/src/.root/handleRequest.js
@@ -1,13 +1,6 @@
-/*
-Global functions:
-- GraphQL (Query GraphQL endpoint)
-- Request (Send a simple http request)
-*/
-
-module.exports = async function handleRequest(req, session) {
+module.exports = async function handleRequest(req) {
   return {
     hello: 'world',
-    req, // Never send unfiltered req to client
-    session // Never send unfiltered or entire session to client
+    req // Never send unfiltered req to client
   };
 }
