d92e9768762e9a633604035aac0d2a81fdbd9b89
Jasper De Moor <jasperdemoor@gmail.com>
2018-07-08 11:47:38 -0700

    Improve error handling

--- packages/example-project/src/.root/js/App.js
+++ packages/example-project/src/.root/js/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return <div className="container">
       <div>
-        <h1>Hello World 📦 🚀</h1>
+        <h1>Hello World! 📦 🚀</h1>
         <h2>{this.props.something}</h2>
       </div>
     </div>;
